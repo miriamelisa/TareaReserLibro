b7fcb099975ede2d17a70db58d5c88926b8d7d1b
miriamelisa <miriam.guanolema1@gmail.com>
2023-10-19 20:24:09 +0200

    CREACION DE PROYECTO RESERVAR LIBORS

---
+++ src/main/java/edu/cesur/fullstack/ProyectFullStackPractica1Application.java
@@ -0,0 +1,13 @@
+package edu.cesur.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectFullStackPractica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectFullStackPractica1Application.class, args);
+	}
+
+}
